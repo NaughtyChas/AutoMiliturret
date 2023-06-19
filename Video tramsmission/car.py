e20242dc63b4f25a002c6f0596c4d59e7a395492
NaughtyChas <3030561972@qq.com>
2023-06-19 14:36:26 +0800

    Code improvement
    Improved client-side code:
      - Realtime video feedback
      - Crash Handlers

--- Video tramsmission/car.py
+++ Video tramsmission/car.py
@@ -1,30 +1,47 @@
 import cv2
-import numpy
 import socket
 import struct
-import time
+# import numpy
+# import time
 
+
+broke = False
+count = 0
 HOST = '10.7.30.16'
 PORT = 9999
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.connect((HOST, PORT))
 print('start sending frames...')
 capture = cv2.VideoCapture(0)
-ret, frame = capture.read()
-cv2.imshow('Video feed', frame)
 
 try:
     while True:
         success, frame = capture.read()
+        count = count + 1
         while not success and frame is None:
             success, frame = capture.read()
-    result, imgencode = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
-    server.sendall(struct.pack('i', imgencode.shape[0]))
-    server.sendall(imgencode)
-    print('single frame sent')
+        cv2.imshow('VideoFeed (Press q to exit)', frame)
+        result, imgencode = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+        server.sendall(struct.pack('i', imgencode.shape[0]))
+        server.sendall(imgencode)
+        print('sent ' + str(count) + ' frames')
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            broke = True
+            break
+
+
 except Exception as e:
     print(e)
-    server.sendall(struct.pack('c', 1))
+    server.sendall(struct.pack('s', b'Error'))
     capture.release()
     server.close()
 
+finally:
+    if broke:
+        print('Transmission interrupted')
+    else:
+        print('Transmission completed')
+
+    capture.release()
+    server.close()
